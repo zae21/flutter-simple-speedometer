ae0a41e2287b279b69c328354c3b7a816379d968
zae21 <prayoga96@gmail.com>
2022-11-23 17:01:56 +0700

    update cached

--- README.md
+++
@@ -1,2 +0,0 @@
-# flutter-simple-speedometer
-Aplikasi Speedometer sederhana dengan flutter
--- android/app/src/main/kotlin/com/example/bg_service/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.bg_service
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
